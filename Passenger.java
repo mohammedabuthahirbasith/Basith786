60f0518d751f3ebc4ac3034b48b7536d192001c7
Mohammed Abuthahir Basith <132328913+mohammedabuthahirbasith@users.noreply.github.com>
2023-09-18 15:13:28 +0530

    Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -1,19 +1,25 @@
 public class Passenger
 {
-    static int id = 1;//static variable to give id for every new passenger
-    String name;
-    int age;
-    String berthPreference;// U or L or M
-    int passengerId;// id of passenger created automatically
-    String alloted;//alloted type (L,U,M,RAC,WL)
-    int number;//seat number
-    public Passenger(String name,int age,String berthPreference)
-    {
-        this.name = name;
-        this.age = age;
-        this.berthPreference = berthPreference;
-        this.passengerId = id++;
-        alloted = "";
-        number = -1;
-    }
+static int id=1;
+String name; //Declaration
+int age;
+String gender;
+String cname;
+int cage;
+String bp;
+int passengerId=id++;
+String alloted;
+int number;
+
+public Passenger(String name, int age, String gender, String cname, int cage, String bp)
+{
+this.name=name;
+this.age=age;
+this.gender=gender;
+this.cname=cname;
+this.cage=cage;
+this.bp=bp;
+alloted=" ";
+number=-1;
+}
 }
